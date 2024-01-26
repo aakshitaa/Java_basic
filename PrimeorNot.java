2e17540cab03323aa1df3d129436fcf75470fdd7
aakshitaa <aakshitasingh786@gmail.com>
2024-01-26 22:12:17 +0530

    optimised approach

--- PrimeorNot.java
+++ PrimeorNot.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class PrimeorNot {
     public static void check(int n){
-        for (int i = 2 ; i < n ; i++){
+        for (int i = 2 ; i < Math.sqrt(n) ; i++){ // optimised approach going to only root of n instead of n
             if (n % i == 0){
             System.out.println("The number is not prime");
             return;
