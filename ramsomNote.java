efa6ff5e46fbc1e531a7e5a6c002e194bd28615e
aakshitaa <aakshitasingh786@gmail.com>
2024-02-12 00:12:58 +0530

    ransomnote fix

--- ramsomNote.java
+++ ramsomNote.java
@@ -1,22 +1,13 @@
 public class ramsomNote {
-    public static boolean canConstruct(String ransomNote, String magazine) {
+    public boolean canConstruct(String ransomNote, String magazine) {
         String temp = new String();
-        int index = -1;
         for (int i =0; i < ransomNote.length(); i++){
             char r = ransomNote.charAt(i);
-            index= -1;
-            for (int j = 0; j < magazine.length(); j++){
-                char m = magazine.charAt(j);
-                if (m == r){
-                    index=  j;
-                    temp = magazine.substring(0,index)+ magazine.substring(index +1);
-                    magazine = temp;
-                }
-
+            int index = magazine.indexOf(r);
+            if (index == -1){
+                return false;
             }
-        }
-        if (index == -1){
-            return false;
+            magazine = magazine.substring(0, index )+  magazine.substring(index + 1);
         }
         return true;
     }
