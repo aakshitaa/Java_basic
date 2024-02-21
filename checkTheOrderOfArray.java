9bc5e64e1708dc555f3d5b7a7dc45a27cb036ab0
aakshitaa <aakshitasingh786@gmail.com>
2024-02-21 21:26:22 +0530

    Checking if given array is sorted in ascending order or not

---
+++ checkTheOrderOfArray.java
@@ -0,0 +1,14 @@
+public class checkTheOrderOfArray {
+    public static boolean check(int arr[]){
+        for(int i = 0; i < arr.length -2; i++){
+            if(arr[i] > arr[i+1]){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        int[] array = new int[]{5, 2,53, 4, 5};
+        System.out.println(check(array));
+    }
+}
